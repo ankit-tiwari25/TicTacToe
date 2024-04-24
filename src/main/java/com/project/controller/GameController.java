25214d5bb44518cb5e9874b960e9d6cf5b30d57f
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-04-25 03:14:56 +0530

    Started with Tic Tac Toe Project

---
+++ src/main/java/com/project/controller/GameController.java
@@ -0,0 +1,20 @@
+package com.project.controller;
+
+import com.project.model.Game;
+import com.project.model.Player;
+import com.project.services.WinningStrategy;
+
+import java.util.List;
+
+public class GameController {
+    public Game createGame(int dimension, List<Player> players, WinningStrategy winningStrategy){
+        return null;
+    }
+    public void displayBoard(Game game){
+
+    }
+
+    public void getGameStatus(Game game){
+
+    }
+}
---
+++ src/main/java/com/project/model/GameStatus.java
@@ -0,0 +1,7 @@
+package com.project.model;
+
+public enum GameStatus {
+    WINNER,
+    DRAW,
+    IN_PROGRESS
+}
